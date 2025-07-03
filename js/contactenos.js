2007eca65b1faf4553ce80dd67edd666d632d348
RodrigoMO123 <93835265+RodrigoMO123@users.noreply.github.com>
2025-07-02 20:27:34 -0500

    Add files via upload

---
+++ js/contactenos.js
@@ -0,0 +1,5 @@
+// Lógica específica para la página Contáctenos
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JS para contactenos.html
+});
---
+++ js/hombres.js
@@ -0,0 +1,5 @@
+// Lógica específica para la página de hombres
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JS para hombres.html
+});
